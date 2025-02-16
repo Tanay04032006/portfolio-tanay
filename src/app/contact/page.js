475abc80a76d6e5d0d8e971cfbc50afe6130fbc6
Tanay Gupta <tanay.gupta13865@gmail.com>
2025-02-16 15:12:32 +0530

    github button on contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,6 +4,8 @@ import React from "react";
 import { FloatingNav } from "../components/Navbar";
 import toast, { Toaster } from "react-hot-toast";
 import Footer from "../components/Footer";
+import Link from "next/link";
+import { IconBrandGithub } from "@tabler/icons-react";
 
 const notify = () => toast.success("Message Sent!");
 
@@ -37,6 +39,18 @@ const ContactPage = () => {
 
   return (
     <div className="bg-black min-h-screen flex flex-col items-center text-white">
+      <div className="fixed top-10 right-4">
+        <Link
+          target="_blank"
+          href="https://github.com/Tanay04032006"
+          className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            Github <IconBrandGithub className="ml-2" />
+          </span>
+        </Link>
+      </div>
       {/* Floating Navigation */}
       <div className="mt-10">
         <FloatingNav />
